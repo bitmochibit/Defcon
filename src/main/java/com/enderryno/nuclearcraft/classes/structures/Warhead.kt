35bd952ec24cfb3c468013f69470e1b0d1940abb
レオナルド <rynosvapos@gmail.com>
2023-06-26 12:05:26 +0200

    O.O

--- src/main/java/com/enderryno/nuclearcraft/classes/structures/Warhead.kt
+++ src/main/java/com/enderryno/nuclearcraft/classes/structures/Warhead.kt
@@ -1,7 +1,11 @@
 package com.enderryno.nuclearcraft.classes.structures
 
+import com.enderryno.nuclearcraft.classes.ExplosiveComponent
 import com.enderryno.nuclearcraft.interfaces.ExplodingStructure
 
-class Warhead : GenericStructure(), ExplodingStructure {
+class Warhead(override val explosiveComponent: ExplosiveComponent) : GenericStructure(), ExplodingStructure {
+    override fun explode() {
+        TODO("Not yet implemented")
+    }
 
 }
