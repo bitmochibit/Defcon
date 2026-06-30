e035aa447cdfc15d1fe8570dfb115f5d2fc5af34
Leonardo Importuna <39531150+bitmochibit@users.noreply.github.com>
2026-06-30 21:06:51 +0200

    jumpscare(actually): mod project
    Signed-off-by: Leonardo Importuna <39531150+bitmochibit@users.noreply.github.com>

---
+++ neoforge/src/main/java/me/mochibit/defcon/mixin/MixinTitleScreen.java
@@ -0,0 +1,19 @@
+package me.mochibit.defcon.mixin;
+
+import me.mochibit.defcon.Constants;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class MixinTitleScreen {
+
+    @Inject(at = @At("HEAD"), method = "init()V")
+    private void init(CallbackInfo info) {
+        Constants.LOG.info("This line is printed by the Defcon mixin from NeoForge!");
+        Constants.LOG.info("MC Version: {}", Minecraft.getInstance().getVersionType());
+    }
+}
